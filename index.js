568e5e5becc4a98dd13a38c443dd50b445dc28dc
Stevan Popovic <stevanpopovic5@gmail.com>
2018-09-10 16:28:16 +0100

    Removed comments and unneeded code from server side.

--- index.js
+++ index.js
@@ -9,15 +9,6 @@ const connection = mysql.createConnection({
   password: 'none',
   database: 'none'
 });
-// Type: MySQL
-// • Host:
-// • Port: 3306
-// • User: none
-// • Password: none
-// • Name: none --> db name
-// • Table: census_learn_sql --> table to request
-
-// console.log(connection);
 
 app.use(express.static(`${__dirname}/public`));
 app.use(function(req, res, next) {
@@ -27,21 +18,14 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', function (req, res) {
-  console.log('in the home backedn req');
   connection.connect();
-  console.log('after connect');
   connection.query('SELECT * FROM census_learn_sql LIMIT 0, 2', function (error, results, fields) {
-    console.log('in the query', results);
-    console.log('res', res);
     if (error) throw error;
     res.json(results);
-    console.log('end');
   });
 
   connection.end();
 });
-
-
 
 app.listen(port, () => console.log(`Express running on port ${port}`));
 module.exports = app;
