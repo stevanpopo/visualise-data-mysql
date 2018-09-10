6a09594a912b2ad575e984a6d3dfd7ebbef0a00e
Stevan Popovic <stevanpopovic5@gmail.com>
2018-09-10 16:25:18 +0100

    Introduced CORS override in server. Getting data from backend.

--- index.js
+++ index.js
@@ -2,7 +2,45 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'none',
+  user: 'none',
+  password: 'none',
+  database: 'none'
+});
+// Type: MySQL
+// • Host:
+// • Port: 3306
+// • User: none
+// • Password: none
+// • Name: none --> db name
+// • Table: census_learn_sql --> table to request
+
+// console.log(connection);
 app.use(express.static(`${__dirname}/public`));
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.get('/', function (req, res) {
+  console.log('in the home backedn req');
+  connection.connect();
+  console.log('after connect');
+  connection.query('SELECT * FROM census_learn_sql LIMIT 0, 2', function (error, results, fields) {
+    console.log('in the query', results);
+    console.log('res', res);
+    if (error) throw error;
+    res.json(results);
+    console.log('end');
+  });
+
+  connection.end();
+});
+
+
 
 app.listen(port, () => console.log(`Express running on port ${port}`));
 module.exports = app;
--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 // import './scss/main.scss';
 
 class App extends React.Component {
@@ -8,8 +9,22 @@ class App extends React.Component {
     this.state = {};
   }
 
+  componentDidMount(){
+    axios({
+      method: 'GET',
+      url: 'http://localhost:4000/',
+      dataType: 'jsonp'
+    })
+      .then(response => {
+        console.log(response);
+        response.json();
+      })
+      // .then(posts => this.setState({ posts }))
+      .catch(err => console.log(err));
+  }
+
   render() {
-    // console.log(this.state);
+    console.log(this.state);
     return (
       <main>
         <h1>Visualize Data React</h1>
